a4bb3ca2faabf03206bd31866d05b4c987663a5a
LanaLizzy-Ivytech <lhendrickson11@ivytech.edu>
2025-04-28 20:03:16 -0400

    Started

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+Author: Lana Hendrickson
+Date: 4/28/2025
+Assigment: Final Project
+Create a program that can accsept a user's inputs on flower oragment and give back their total
+'''
+
+#import
+from breezypythongui import EasyFrame
+
+class window(EasyFrame):
+    
+    def __init__(self):
+        EasyFrame.__init__(self, title = "Rosebud Online Flower Shop")
+        #need to reserch to fix buttons.
+
+        
+      
+
+def main():
+    window().mainloop()
+    
+if __name__ == "__main__":
+    main()
